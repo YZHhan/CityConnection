65ecf9d57a3c51412a3818406dcba588e30893e6
YZHhan <214939009@qq.comm>
2017-05-19 10:45:53 +0800

    0519 HearderNews conract

---
+++ app/src/main/java/com/han/cityconnection/CityPart/HeardNews/contract/HeaderNewContract.java
@@ -0,0 +1,32 @@
+package com.han.cityconnection.CityPart.HeardNews.contract;
+
+import com.han.cityconnection.CityPart.HeardNews.Entity.BeiJingMsgList;
+import com.han.cityconnection.CityPart.HeardNews.Entity.CityNewsInfoBean;
+import com.han.cityconnection.base.BaseView;
+
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2017/5/9.
+ */
+
+public interface HeaderNewContract {
+
+    interface HeaderView extends BaseView {
+        //加载数据   需传入参数 参数为返回的JavaBean
+        void setData(BeiJingMsgList beiJingMsgList);
+    }
+    interface HeaderNewsView extends BaseView {
+        //加载数据   需传入参数 参数为返回的JavaBean
+        void setData(CityNewsInfoBean cityNewsInfoBean);
+    }
+
+
+    interface HeaderPresenter {
+        //将view层传入的地址在次转入到model层，获取网络数据
+        void loadNetData(String url, Map<String, String> params);
+        void loadNewsData(String url, Map<String, String> params);
+
+
+    }
+}
