c21da246e62fc2d51793a0e54da8492303d65c26
YZHhan <214939009@qq.comm>
2017-05-19 14:57:16 +0800

    0519 model headerModel

---
+++ app/src/main/java/com/han/cityconnection/persenteer/header/HeadNewsInfoParImpl.java
@@ -0,0 +1,8 @@
+package com.han.cityconnection.persenteer.header;
+
+/**
+ * Created by Administrator on 2017/5/16.
+ */
+
+public class HeadNewsInfoParImpl  {
+}
---
+++ app/src/main/java/com/han/cityconnection/persenteer/job/Action.java
@@ -0,0 +1,8 @@
+package com.han.cityconnection.persenteer.job;
+
+/**
+ * Created by Administrator on 2017/5/10.
+ */
+
+public class Action {
+}
